daab5f95c3db3bade046aa9abc0d3cd2b3141111
Sindhu Rallabhandi <145177344+prpl-25@users.noreply.github.com>
2025-01-13 14:18:41 -0700

    Connecting to database

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,17 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import { connectDB } from './config/db.js';
+
+dotenv.config();
 
 const app = express();
 
 app.get("/home", (req, res) => {
     res.send("Server is ready123");
 });
+console.log(process.env.MONGO_URI);
 
 app.listen(8888, () =>{
+    connectDB();
     console.log("Server started at http://localhost:8888 hi muna");
 });
