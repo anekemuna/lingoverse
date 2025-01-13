72bcc15937230a73f9b6707ad179e017388e96ed
Sindhu Rallabhandi <145177344+prpl-25@users.noreply.github.com>
2025-01-13 13:49:38 -0700

    Made get method (testing)

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,10 @@ import express from 'express';
 
 const app = express();
 
+app.get("/products", (req, res) => {
+    res.send("Server is ready123");
+});
+
 app.listen(8888, () =>{
     console.log("Server started at http://localhost:8888 hi muna");
 });
