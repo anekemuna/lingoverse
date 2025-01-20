b1bad31564a808373dfab21d6c48ac615bccb165
Sindhu Rallabhandi <145177344+prpl-25@users.noreply.github.com>
2025-01-20 13:30:06 -0700

    password hashing added - bcrypt

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,11 +1,14 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 // schema for users collections
 const userSchema = new mongoose.Schema(
     {
         userName:{
             type: String,
-            required: true
+            required: true,
+            unique: true,
+            lowercase: true
         },
         firstName:{
             type: String,
@@ -17,7 +20,9 @@ const userSchema = new mongoose.Schema(
         },
         email:{
             type: String,
-            required: false
+            required: false,
+            unique: true,
+            lowercase: true
         },
         password:{
             type: String,
@@ -30,5 +35,15 @@ const userSchema = new mongoose.Schema(
 );
 
 const User = mongoose.model("User", userSchema);
+
+//wait for next() and then save the password to database
+userSchema.pre('save', async function (next){
+    //if password is not modified, return next() to continue
+    if(!this.isModified('password')) return next();
+    //else hash the password and then return next()
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
 
 export default User;
