76c6df458c81b77dfc9f1331a32135a0bf8089ab
Sindhu Rallabhandi <145177344+prpl-25@users.noreply.github.com>
2025-01-24 13:12:10 -0700

    login API working and ready to go!

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,15 +1,16 @@
 import mongoose from "mongoose";
 import User from "../models/user.model.js";
 import { hashPassword } from '../utils/hashPassword.js';
+import { verifyUser } from "../utils/verifyUser.js";
 
 export const registerUser = async (req,res) => {
-    const user = req.body;
-    if(!user.userName || !user.firstName || !user.email || !user.password){
+    const requestObj = req.body;
+    if(!requestObj.userName || !requestObj.firstName || !requestObj.email || !requestObj.password){
         res.status(400).json({success: false, message: 'Please provide all details'})
     }
     //hashing the password before adding the user to database
-    const newUser = new User(user);
-    await hashPassword(user.password)
+    const newUser = new User(requestObj);
+    await hashPassword(requestObj.password)
         .then(hashed => newUser.password = hashed)
         .catch(err => console.error(err));
     try{
@@ -20,6 +21,27 @@ export const registerUser = async (req,res) => {
         res.status(500).json({success: false, message: "Server Error"});
     }
 };
-export const loginUser = async (req, res) => {};
+export const loginUser = async (req, res) => {
+    const requestObj = req.body;
+    //check for empty username or password
+    if(!requestObj.userName || !requestObj.password){
+        res.status(400).json({success: false, message: 'Please provide all details'})
+    }
+    try{
+        //method to verify username email and password from database
+        const userIsValid = await verifyUser(requestObj.userName, requestObj.password)
+        console.log(userIsValid)
+        //check if matches
+        if(userIsValid){
+            return res.status(200).json({succes: true, message: 'Login successful'})
+        }
+        return res.status(401).json({success: false, message: 'Invalid Login Credentials'})
+    }
+    catch(error){
+        console.error("Error in logging user in:", error.message);
+        res.status(500).json({success: false, message: "Server Error"});
+    }
+};
 export const updateUser = async (req, res) => {};
 export const deleteUser = async (req, res) => {};
+
