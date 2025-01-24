5600aa2690ba1cd599d90261f8ec8de947b78ad9
Munachimso Aneke <52995219+anekemuna@users.noreply.github.com>
2025-01-24 11:52:35 -0700

    Fixed Bcrypt Hashing, Registration API complete

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,12 +1,17 @@
 import mongoose from "mongoose";
 import User from "../models/user.model.js";
+import { hashPassword } from '../utils/hashPassword.js';
 
 export const registerUser = async (req,res) => {
     const user = req.body;
     if(!user.userName || !user.firstName || !user.email || !user.password){
         res.status(400).json({success: false, message: 'Please provide all details'})
     }
+    //hashing the password before adding the user to database
     const newUser = new User(user);
+    await hashPassword(user.password)
+        .then(hashed => newUser.password = hashed)
+        .catch(err => console.error(err));
     try{
         await newUser.save();
         res.status(201).json({success: true, data: newUser});
--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,5 +1,4 @@
 import mongoose from "mongoose";
-//import bcrypt from "bcrypt";
 
 // schema for users collections
 const userSchema = new mongoose.Schema(
@@ -35,15 +34,5 @@ const userSchema = new mongoose.Schema(
 );
 
 const User = mongoose.model("User", userSchema);
-
-//wait for next() and then save the password to database
-// userSchema.pre('save', async function (next){
-//     //if password is not modified, return next() to continue
-//     if(!this.isModified('password')) return next();
-//     //else hash the password and then return next()
-//     const salt = await bcrypt.genSalt(10);
-//     this.password = await bcrypt.hash(this.password, salt);
-//     next();
-// });
 
 export default User;
