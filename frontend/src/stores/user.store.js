eb7246d89eee85f53219cbdc48d1389605f83115
Munachimso Aneke <52995219+anekemuna@users.noreply.github.com>
2025-03-17 11:19:52 -0700

    removed unnecessary comments

--- frontend/src/stores/user.store.js
+++ frontend/src/stores/user.store.js
@@ -1,36 +1,5 @@
 import { create } from "zustand";
 
-// export const useUserStore = create((set, get) => ({
-//   user: null,
-//   loading: false,
-
-//   login: async (username, password) => {
-//     set({ loading: true });
-//     try {
-//       const res = await fetch("/home/login", {
-//         method: "POST",
-//         headers: {
-//             "Content-Type": "application/json",
-//         },
-//         body: JSON.stringify({
-//             userName: username,
-//             password: password,
-//         })
-//       });
-//       const data = await res.json();
-//       set({ user: data.data });
-//       if (data.success) {
-//         console.log("User details:", data.user); // Print user details
-//         set({ user: data.user, loading: false });
-//       }
-//     //   console.log(data.success)
-//     } catch (error) {
-//       set({ loading: false });
-//       console.error(error);
-//     }
-//   },
-// }));
-
 export const useUserStore = create((set) => ({
     user: null,
     loading: false,
